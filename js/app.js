740d3f581965ccd437f0bfb38ac2488990fd8c20
John Stratton <jstrat0407@gmail.com>
2018-06-06 14:40:59 +0000

    added click handler to upload button

--- js/app.js
+++ js/app.js
@@ -1,27 +1,29 @@
 // replace input placeholder with file name
-$('#inputGroupFile02').on('change',function(){
-  let fileName = $(this).val();
-  fileName = fileName.substring(fileName.lastIndexOf('\\') + 1);
-  $(this).next('.custom-file-label').addClass("selected").html(fileName);
+$('#inputGroupFile02').on('change', function () {
+	let fileName = $(this).val();
+	fileName = fileName.substring(fileName.lastIndexOf('\\') + 1);
+	$(this).next('.custom-file-label').addClass("selected").html(fileName);
 })
+
+// click handler for #upload
+$('#upload').click(function () {
+	console.log('clicked');
+});
 
 // select files using jQuery
 $('input[type=file]').parse({
 	config: {
 		// base config to use for each file
 	},
-	before: function(file, inputElem)
-	{
+	before: function (file, inputElem) {
 		// executed before parsing each file begins;
 		// what you return here controls the flow
 	},
-	error: function(err, file, inputElem, reason)
-	{
+	error: function (err, file, inputElem, reason) {
 		// executed if an error occurs while loading the file,
 		// or if before callback aborted for some reason
 	},
-	complete: function()
-	{
+	complete: function () {
 		// executed after all files are complete
 	}
 });
