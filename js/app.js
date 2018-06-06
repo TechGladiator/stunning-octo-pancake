0da7081532e66b9905646b6a38826d0f03db3265
John Stratton <jstrat0407@gmail.com>
2018-06-06 15:38:46 +0000

    added default PapaParser configuration

--- js/app.js
+++ js/app.js
@@ -8,11 +8,32 @@ $('#inputGroupFile02').on('change', function () {
 // click handler for #upload
 $('#upload').click(function () {
 	console.log('clicked');
-	
+
 	// select files using jQuery
 	$('input[type=file]').parse({
 		config: {
 			// base config to use for each file
+			delimiter: "", // auto-detect
+			newline: "", // auto-detect
+			quoteChar: '"',
+			escapeChar: '"',
+			header: false,
+			trimHeader: false,
+			dynamicTyping: false,
+			preview: 0,
+			encoding: "",
+			worker: false,
+			comments: false,
+			step: undefined,
+			complete: undefined,
+			error: undefined,
+			download: false,
+			skipEmptyLines: false,
+			chunk: undefined,
+			fastMode: undefined,
+			beforeFirstChunk: undefined,
+			withCredentials: undefined,
+			transform: undefined
 		},
 		before: function (file, inputElem) {
 			// executed before parsing each file begins;
