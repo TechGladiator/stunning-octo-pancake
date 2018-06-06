065d91c59602b7ec450e8538f9a0cad58873f439
John Stratton <jstrat0407@gmail.com>
2018-06-06 22:27:45 +0000

    cleaned up and removed unused code from app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,18 @@
-var inputType = "string";
-var stepped = 0,
-	rowCount = 0,
-	errorCount = 0,
-	fileName,
-	firstError;
-var start, end;
-var firstRun = true;
-var maxUnparseLength = 10000;
+let stepped = 0;
+let rowCount = 0;
+let errorCount = 0;
+let firstError;
+let start;
+let end;
+let firstRun = true;
 
 function printStats(msg) {
-	if (msg)
-		console.log(msg);
-	console.log("       Time:", (end - start || "(Unknown; your browser does not support the Performance API)"), "ms");
+	if (msg) console.log(msg);
+	console.log("       Time:", end - start || "(Unknown; your browser does not support the Performance API)", "ms");
 	console.log("  Row count:", rowCount);
-	if (stepped)
-		console.log("    Stepped:", stepped);
+	if (stepped) console.log("    Stepped:", stepped);
 	console.log("     Errors:", errorCount);
-	if (errorCount)
-		console.log("First error:", firstError);
+	if (errorCount) console.log("First error:", firstError);
 }
 
 function buildConfig() {
@@ -32,16 +27,14 @@ function buildConfig() {
 		worker: $('#worker').prop('checked'),
 		comments: $('#comments').val(),
 		complete: completeFn,
-		error: errorFn,
-		download: inputType == "remote"
+		error: errorFn
 	};
 }
 
 function stepFn(results, parser) {
 	stepped++;
 	if (results) {
-		if (results.data)
-			rowCount += results.data.length;
+		if (results.data) rowCount += results.data.length;
 		if (results.errors) {
 			errorCount += results.errors.length;
 			firstError = firstError || results.errors[0];
@@ -57,8 +50,7 @@ function completeFn(results) {
 			errorCount = results.errors.length;
 			firstError = results.errors[0];
 		}
-		if (results.data && results.data.length > 0)
-			rowCount = results.data.length;
+		if (results.data && results.data.length > 0) rowCount = results.data.length;
 	}
 
 	printStats("Parse complete");
@@ -79,37 +71,27 @@ function enableButton() {
 }
 
 function now() {
-	return typeof window.performance !== 'undefined' ?
-		window.performance.now() :
-		0;
+	return typeof window.performance !== 'undefined' ? window.performance.now() : 0;
 }
 
-$(function () {
+$(() => {
 	// Demo invoked
 	$('#upload').click(function () {
-		if ($(this).prop('disabled') == "true")
-			return;
+		if ($(this).prop('disabled') == "true") return;
 
 		stepped = 0;
 		rowCount = 0;
 		errorCount = 0;
 		firstError = undefined;
 
-		var config = buildConfig();
-		var input = $('#inputGroupFile02').val();
-
-		if (inputType == "remote")
-			input = $('#url').val();
-		else if (inputType == "json")
-			input = $('#json').val();
+		const config = buildConfig();
+		const input = $('#inputGroupFile02').val();
 
 		// Allow only one parse at a time
 		$(this).prop('disabled', true);
 
-		if (!firstRun)
-			console.log("--------------------------------------------------");
-		else
-			firstRun = false;
+		if (!firstRun) console.log("--------------------------------------------------");
+		else firstRun = false;
 
 		if (!$('#inputGroupFile02')[0].files.length) {
 			alert("Please choose at least one file to parse.");
@@ -117,17 +99,17 @@ $(function () {
 		}
 
 		$('#inputGroupFile02').parse({
-			config: config,
-			before: function (file, inputElem) {
+			config,
+			before: function before(file, inputElem) {
 				start = now();
 				console.log("Parsing file...", file);
 			},
-			error: function (err, file) {
+			error: function error(err, file) {
 				console.log("ERROR:", err, file);
 				firstError = firstError || err;
 				errorCount++;
 			},
-			complete: function () {
+			complete: function complete() {
 				end = now();
 				printStats("Done with all files");
 			}
@@ -137,7 +119,7 @@ $(function () {
 
 // replace input placeholder with file name
 $('#inputGroupFile02').on('change', function () {
-	fileName = $(this).val();
+	let fileName = $(this).val();
 	fileName = fileName.substring(fileName.lastIndexOf('\\') + 1);
 	$(this).next('.custom-file-label').addClass("selected").html(fileName);
-})
+});
