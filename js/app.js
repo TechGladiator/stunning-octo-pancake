9c2494bd4cac04a72ea0197dbeb78a0a266f218a
John Stratton <jstrat0407@gmail.com>
2018-06-06 15:23:49 +0000

    moved file select scaffolding to click handler

--- js/app.js
+++ js/app.js
@@ -8,22 +8,22 @@ $('#inputGroupFile02').on('change', function () {
 // click handler for #upload
 $('#upload').click(function () {
 	console.log('clicked');
-});
-
-// select files using jQuery
-$('input[type=file]').parse({
-	config: {
-		// base config to use for each file
-	},
-	before: function (file, inputElem) {
-		// executed before parsing each file begins;
-		// what you return here controls the flow
-	},
-	error: function (err, file, inputElem, reason) {
-		// executed if an error occurs while loading the file,
-		// or if before callback aborted for some reason
-	},
-	complete: function () {
-		// executed after all files are complete
-	}
+	
+	// select files using jQuery
+	$('input[type=file]').parse({
+		config: {
+			// base config to use for each file
+		},
+		before: function (file, inputElem) {
+			// executed before parsing each file begins;
+			// what you return here controls the flow
+		},
+		error: function (err, file, inputElem, reason) {
+			// executed if an error occurs while loading the file,
+			// or if before callback aborted for some reason
+		},
+		complete: function () {
+			// executed after all files are complete
+		}
+	});
 });
