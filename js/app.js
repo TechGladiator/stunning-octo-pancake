6f9ee7a3f29100fb82114074ba8eb6d766347d51
John Stratton <jstrat0407@gmail.com>
2018-06-11 10:28:15 -0400

    log whether a state is valid or not

--- js/app.js
+++ js/app.js
@@ -47,13 +47,20 @@ function completeFn(results) {
 	}
 
 	function validate(field) {
+
+		fieldState = false;
+
 		const states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
-		if (field) {
-			
-		}
 		for (let i = 0; i < states.length; i++) {
-			
+			if(field.State.toUpperCase() == states[i]) {
+				console.log(`${field.State} is valid`);
+				fieldState = true;
+			}
+		}
+
+		if (!fieldState) {
+			console.log(`${field.State} is invalid`);
 		}
 	}
 	
@@ -61,6 +68,7 @@ function completeFn(results) {
 		for (let i = 0; i < fieldData.length; i++) {
 			const e = fieldData[i];
 			console.log(e);
+			validate(e);
 			fields += `
 				<tr>
 					<th scope="row">${i + 1}</th>
@@ -68,7 +76,6 @@ function completeFn(results) {
 			for (const k in e) {
 				if (e.hasOwnProperty(k)) {
 					const f = e[k];
-					console.log(f);
 					fields += `<td>${f}</td>`;
 				}
 			}
