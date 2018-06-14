9332eb9920581445107a042452b896f2f513a584
John Stratton <jstrat0407@gmail.com>
2018-06-14 10:04:57 -0400

    refactored code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // global variables
+let columnHeads = '';
 let end;
 let errorCount = 0;
 let fieldData;
 let fieldDate = true;
 let fieldCount;
 let fieldNames;
+let fields = '';
 let fieldState = false;
 let fieldZip = true;
 let fileName;
@@ -17,6 +19,12 @@ const names = ['Name', 'Address', 'Address 2', 'City', 'State', 'Zip', 'Purpose'
 let rowCount = 0;
 let start;
 
+// replace input placeholder with file name
+$('#inputGroupFile02').on('change', function () {
+	fileName = $(this).val();
+	fileName = fileName.substring(fileName.lastIndexOf('\\') + 1);
+	$(this).next('.custom-file-label').addClass('selected').html(fileName);
+});
 
 function printStats(msg) {
 	if (msg) console.log(msg);
@@ -112,87 +120,45 @@ function validateDate(field) {
 	}
 }
 
-function completeFn(results) {
-	end = now();
-
-	fieldNames = results.meta.fields;
-	fieldData = results.data;
-	
-	validateRowLength(fieldNames);
-	
-	// capture errors
-	if (results && results.errors) {
-		if (results.errors) {
-			let code;
-			let codeMsg;
-			if (lengthHigh || lengthLow) {
-				if (lengthHigh) {
-					code = "TooManyFields";
-					codeMsg = "Too Many Fields: ";
-				} else if (lengthLow) {
-					code = "TooFewFields";
-					codeMsg = "Too Few Fields: ";
-				}
-				errorCount = results.errors.length + 1;
-				firstError = {
-					"type": "FieldMismatch",
-					"code": code,
-					"message": `${codeMsg}expected ${names.length} but parsed ${fieldNames.length}`,
-					"row": 0
-				}
-			} else {
-				errorCount = results.errors.length;
-				firstError = results.errors[0];
-			}
-		}
-		if (results.data && results.data.length > 0) rowCount = results.data.length;
-	}
-
-	printStats('Parse complete');
-	console.log('    Results:', results);
-
-	let columnHeads = '';
-	let fields = '';
-
-	function getFieldNames() {
-		for (let i = 0; i < fieldNames.length; i++) {
-			validateFieldNames(fieldNames[i]);
-			if (!name) {
-				columnHeads += `<th scope="col" class="table-danger">${fieldNames[i]}</th>`;
-			} else {
-				columnHeads += `<th scope="col">${fieldNames[i]}</th>`;
-			}
+function getFieldNames() {
+	for (let i = 0; i < fieldNames.length; i++) {
+		validateFieldNames(fieldNames[i]);
+		if (!name) {
+			columnHeads += `<th scope="col" class="table-danger">${fieldNames[i]}</th>`;
+		} else {
+			columnHeads += `<th scope="col">${fieldNames[i]}</th>`;
 		}
 	}
+}
 
-	function getFieldData() {
-		for (let i = 0; i < fieldData.length; i++) {
-			const e = fieldData[i];
-			validateState(e);
-			validateZip(e);
-			validateDate(e);
-			fields += `
-				<tr>
-					<th scope="row">${i + 1}</th>
-			`;
-			for (const k in e) {
-				if (e.hasOwnProperty(k)) {
-					const f = e[k];
-					if (f == e.State && !fieldState || f == e.Zip && !fieldZip || f == e['Creation Date'] && !fieldDate) {
-						fields += `<td class="table-danger">${f}</td>`
-					} else {
-						fields += `<td>${f}</td>`;
-					}
+function getFieldData() {
+	for (let i = 0; i < fieldData.length; i++) {
+		const e = fieldData[i];
+		validateState(e);
+		validateZip(e);
+		validateDate(e);
+		fields += `
+			<tr>
+				<th scope="row">${i + 1}</th>
+		`;
+		for (const k in e) {
+			if (e.hasOwnProperty(k)) {
+				const f = e[k];
+				if (f == e.State && !fieldState || f == e.Zip && !fieldZip || f == e['Creation Date'] && !fieldDate) {
+					fields += `<td class="table-danger">${f}</td>`
+				} else {
+					fields += `<td>${f}</td>`;
 				}
 			}
-			fields += `</tr>`;
 		}
+		fields += `</tr>`;
 	}
+}
 
-	if (errorCount == 0) {
-		getFieldNames();
-		getFieldData();
-		$('.csv').html(`
+function buildTable() {
+	getFieldNames();
+	getFieldData();
+	$('.csv').html(`
 		<div class="card">
 			<div class="card-body">
 				<table class="table table-bordered">
@@ -208,7 +174,56 @@ function completeFn(results) {
 				</table>
 			</div>
 		</div>
-		`);
+	`);
+}
+
+function errorCap(results) {
+	if (results && results.errors) {
+		if (results.errors) {
+			let code;
+			let codeMsg;
+			if (lengthHigh || lengthLow) {
+				if (lengthHigh) {
+					code = "TooManyFields";
+					codeMsg = "Too Many Fields: ";
+				}
+				else if (lengthLow) {
+					code = "TooFewFields";
+					codeMsg = "Too Few Fields: ";
+				}
+				errorCount = results.errors.length + 1;
+				firstError = {
+					"type": "FieldMismatch",
+					"code": code,
+					"message": `${codeMsg}expected ${names.length} but parsed ${fieldNames.length}`,
+					"row": 0
+				};
+			}
+			else {
+				errorCount = results.errors.length;
+				firstError = results.errors[0];
+			}
+		}
+		if (results.data && results.data.length > 0)
+			rowCount = results.data.length;
+	}
+}
+
+function completeFn(results) {
+	end = now();
+
+	fieldNames = results.meta.fields;
+	fieldData = results.data;
+	
+	validateRowLength(fieldNames);
+	
+	errorCap(results);
+
+	printStats('Parse complete');
+	console.log('    Results:', results);
+
+	if (errorCount == 0) {
+		buildTable();
 	} else {
 		$('.csv').html('');
 	}
@@ -235,58 +250,56 @@ function buildConfig() {
 	};
 }
 
-$(() => {
-	// Demo invoked
-	$('#upload').click(function () {
+function getRowNumb(row) {
+	if (firstError.row != 0 || !lengthHigh && !lengthLow) {
+		row = JSON.stringify(firstError.row + 2);
+	}
+	else {
+		row = JSON.stringify(firstError.row + 1);
+	}
+	return row;
+}
 
+function parseFile(config) {
+	$('#inputGroupFile02').parse({
+		config,
+		before: function before(file) {
+			start = now();
+			console.log('Parsing file...', file);
+		},
+		error: function error(err, file) {
+			console.log('ERROR:', err, file);
+			firstError = firstError || err;
+			errorCount++;
+		},
+		complete: function complete() {
+			end = now();
+			printStats("Done with all files");
+			if (firstError) {
+				let errorMsg = JSON.stringify(firstError.message);
+				let row;
+				row = getRowNumb(row);
+				modal(`${errorMsg.replace(/['"]+/g, '')} ${fileName} Row: ${row}`);
+			}
+		}
+	});
+}
+
+function beginParsing() {
+	$('#upload').click(function () {
 		rowCount = 0;
 		errorCount = 0;
 		firstError = undefined;
-
 		const config = buildConfig();
-		const input = $('#inputGroupFile02').val();
-
-		if (!firstRun) console.log("--------------------------------------------------");
-		else firstRun = false;
-
+		if (!firstRun)
+			console.log("--------------------------------------------------");
+		else
+			firstRun = false;
 		if (!$('#inputGroupFile02')[0].files.length) {
 			modal('Please choose at least one file to parse');
 		}
-
-		$('#inputGroupFile02').parse({
-			config,
-			before: function before(file) {
-				start = now();
-				console.log('Parsing file...', file);
-			},
-			error: function error(err, file) {
-				console.log('ERROR:', err, file);
-				firstError = firstError || err;
-				errorCount++;
-			},
-			complete: function complete() {
-				end = now();
-				printStats("Done with all files");
-				if (firstError) {
-					let errorMsg = JSON.stringify(firstError.message);
-					let row;
-					if (firstError.row != 0 || !lengthHigh && !lengthLow) {
-						row = JSON.stringify(firstError.row + 2);
-					} else {
-						row = JSON.stringify(firstError.row + 1);
-					}
-					modal(`${errorMsg.replace(/['"]+/g, '')} ${fileName} Row: ${row}`);
-				}
-			}
-		});
-
-
+		parseFile(config);
 	});
-});
+}
 
-// replace input placeholder with file name
-$('#inputGroupFile02').on('change', function () {
-	fileName = $(this).val();
-	fileName = fileName.substring(fileName.lastIndexOf('\\') + 1);
-	$(this).next('.custom-file-label').addClass('selected').html(fileName);
-});
+beginParsing();
