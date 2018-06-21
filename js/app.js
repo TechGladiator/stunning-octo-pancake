8260954c8d41d9ccae241d1505775a0c7d34a3f7
John Stratton <jstrat0407@gmail.com>
2018-06-21 10:16:02 -0400

    clear .csv when a new file is selected
    it was bugging me

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ $('#inputGroupFile02').on('change', function () {
 	fileName = fileName.substring(fileName.lastIndexOf('\\') + 1);
 	if (fileName != '') {
 		$(this).next('.custom-file-label').addClass('selected').html(fileName);
+		$('.csv').html('');
 	} else {
 		$(this).next('.custom-file-label').addClass('selected').html('Drag & Drop or click here to browse for your file');
 		$('.csv').html('');
