e49d301061b438c6bace4f97e45349c49d34544c
John Stratton <jstrat0407@gmail.com>
2018-06-24 17:09:05 -0400

    display header row

--- js/fixError.js
+++ js/fixError.js
@@ -2,5 +2,28 @@ function fixRow(code, close, row) {
   $(`#${code}${close}`).click(() => {
     console.log('Row: ', row);
     console.log(fieldData[row]);
+    let fn = '';
+    let fd = '';
+    fieldNames.forEach(e => {
+      console.log(e);
+      fn += `<th>${e}</th>`;
+    })
+    $('.csv').html(`
+                    <div class="card">
+                      <div class="card-body">
+                        <table class="table table-bordered">
+                          <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              ${fn}
+                            </tr>
+                          </thead>
+                          <tbody>
+
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  `);
   });
 }
