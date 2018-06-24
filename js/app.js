ac480d631a69941ffa879b3a9e0b6308e9698622
John Stratton <jstrat0407@gmail.com>
2018-06-23 20:05:30 -0400

    added global variables from demo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// global variables
+var inputType = "string";
+var stepped = 0, rowCount = 0, errorCount = 0, firstError;
+var start, end;
+var firstRun = true;
+var maxUnparseLength = 10000;
+
 // Users navigate to web site and upload a CSV file
 // replace input placeholder with file name
 $('#inputGroupFile02').on('change', function () {
