6dd054426eff25a5015210311ffe53e0b9190418
John Stratton <jstrat0407@gmail.com>
2018-06-24 18:26:56 -0400

    added click handlers to button group

--- js/fixError.js
+++ js/fixError.js
@@ -1,5 +1,11 @@
 function fixRow(code, close, row) {
   $(`#${code}${close}`).click(() => {
     buildTable(row, buttonGroup);
+    $('#editData').click(() => {
+      console.log('you clicked Edit Data');
+    });
+    $('#cancelCSV').click(() => {
+      console.log('you clicked Cancel CSV Processing');
+    });
   });
 }
