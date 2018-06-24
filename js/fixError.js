b4c74ea0fc2b07f4f102c32640b68cc8154d5bc8
John Stratton <jstrat0407@gmail.com>
2018-06-24 19:20:23 -0400

    moved toggling editable state to makeEditable()

--- js/fixError.js
+++ js/fixError.js
@@ -1,19 +1,24 @@
+function makeEditable() {
+  const editable = $('#headings, #dataEntered');
+  console.log('editable = ', editable);
+  if (!editable[0].isContentEditable) {
+    editable[0].contentEditable = 'true';
+    editable[1].contentEditable = 'true';
+    $('#editData').html('Save Edits');
+  }
+  else {
+    editable[0].contentEditable = 'false';
+    editable[1].contentEditable = 'false';
+    $('#editData').html('Edit Data');
+  }
+}
+
 function fixRow(code, close, row) {
   $(`#${code}${close}`).click(() => {
     buildTable(row, buttonGroup);
     $('#editData').click(() => {
       console.log('you clicked Edit Data');
-      const editable = $('#headings, #dataEntered');
-      console.log('editable = ', editable);
-      if (!editable[0].isContentEditable) {
-        editable[0].contentEditable = 'true';
-        editable[1].contentEditable = 'true';
-        $('#editData').html('Save Edits');
-      } else {
-        editable[0].contentEditable = 'false';
-        editable[1].contentEditable = 'false';
-        $('#editData').html('Edit Data');
-      }
+      makeEditable();
     });
     $('#cancelCSV').click(() => {
       console.log('you clicked Cancel CSV Processing');
