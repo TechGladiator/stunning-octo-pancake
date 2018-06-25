bd903afaec626e08b47140c292c8b523bb37852d
John Stratton <jstrat0407@gmail.com>
2018-06-24 21:46:02 -0400

    moved field updating to updateFields()

--- js/fixError.js
+++ js/fixError.js
@@ -13,21 +13,25 @@ function makeEditable() {
   }
 }
 
+function updateFields(row) {
+  for (let i = 0; i < fieldNames.length; i++) {
+    fieldNames[i] = $(`#header${i}`).html();
+    console.log(fieldNames[i]);
+  }
+  let j = 0;
+  for (const k in fieldData[row]) {
+    if (fieldData[row].hasOwnProperty(k)) {
+      fieldData[row][k] = $(`#field${j}`).html();
+      console.log(fieldData[row][k]);
+      j++;
+    }
+  }
+}
+
 function buttonGroupClicks(row) {
   $('#editData').click(() => {
     makeEditable();
-    for (let i = 0; i < fieldNames.length; i++) {
-      fieldNames[i] = $(`#header${i}`).html();
-      console.log(fieldNames[i]);
-    }
-    let j = 0;
-    for (const k in fieldData[row]) {
-      if (fieldData[row].hasOwnProperty(k)) {
-        fieldData[row][k] = $(`#field${j}`).html();
-        console.log(fieldData[row][k]);
-        j++;
-      }
-    }
+    updateFields(row);
   });
   $('#repairNext').click(() => {
     console.log('you clicked Repair Next Error');
@@ -36,7 +40,6 @@ function buttonGroupClicks(row) {
     console.log('you clicked Cancel CSV Processing');
   });
 }
-
 
 function fixRow(code, close, row) {
   $(`#${code}${close}`).click(() => {
