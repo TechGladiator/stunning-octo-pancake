b925d967f03c6c4f7716e2e9d0ee497d8ed207ad
John Stratton <jstrat0407@gmail.com>
2018-06-24 23:40:11 -0400

    clear empty fields and remove first error message

--- js/fixError.js
+++ js/fixError.js
@@ -43,6 +43,25 @@ function buttonGroupClicks(row) {
 
 function fixRow(code, close, row) {
   $(`#${code}${close}`).click(() => {
+    for (const k in fieldData[row]) {
+      if (fieldData[row].hasOwnProperty(k)) {
+        console.log(`${k} : ${fieldData[row][k]}`);
+        if (k != 'Address 2' && fieldData[row][k] == '') {
+          console.log('k != \'Address 2\'');
+          console.log('delete');
+          delete fieldData[row][k];
+        }
+      }
+    }
+    $(`.modal`).on('hidden.bs.modal', () => {
+      if (Object.values(fieldData[row]).length == fieldNames.length) {
+        fieldErrors.shift(0);
+        console.log(fieldErrors);
+        errorCount--;
+        firstError = fieldErrors[0];
+        console.log('First Error:', firstError);
+      }
+    });
     buildTable(row, buttonGroup);
     buttonGroupClicks(row);
   });
