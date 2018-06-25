49d3b22d513315e08c8f69f53cce36435bffe383
John Stratton <jstrat0407@gmail.com>
2018-06-25 13:27:27 -0400

    extracted global variables into globals.js

--- js/parse.js
+++ js/parse.js
@@ -1,26 +1,3 @@
- // global variables
- const buttonGroup = `
-	<div class="btn-group d-flex justify-content-center mb-3" role="group" aria-label="button group">
-		<button type="button" class="btn btn-secondary" id="editData">Edit Data</button>
-		<button type="button" class="btn btn-secondary" id="repairNext">Repair Next Error</button>
-		<button type="button" class="btn btn-secondary" id="cancelCSV">Cancel CSV Processing</button>
-	</div>
-`;
- const names = ['Name', 'Address', 'Address 2', 'City', 'State', 'Zip', 'Purpose', 'Property Owner', 'Creation Date'];
- let code;
- let message;
- let row;
- let rowCount = 0;
- let errorCount = 0;
- let fieldData;
- let fieldErrors;
- let fieldNames;
- let firstError;
- let fullResults;
- let start;
- let end;
- let firstRun = true;
-
  // Users navigate to web site and upload a CSV file
  // replace input placeholder with file name
  $('#inputGroupFile02').on('change', function () {
