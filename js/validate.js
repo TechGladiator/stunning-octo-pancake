2896a4592c6fb58722d1915278d3af396efa51e1
John Stratton <jstrat0407@gmail.com>
2018-06-25 16:02:52 -0400

    added validateFieldNames()

--- js/validate.js
+++ js/validate.js
@@ -1,3 +1,19 @@
+// Validate field names
+function validateFieldNames(fieldName) {
+	for (let i = 0; i < names.length; i++) {
+		if (fieldName == names[i]) {
+			name = true;
+			return;
+		} else {
+			name = false;
+		}
+	}
+	if (!name) {
+		console.log(`Header "${fieldName}" is invalid`);
+    modal('errorAlert', `Header "${fieldName}" is invalid`);
+	}
+}
+
 // Validate data type formats in each record
 function validate(something) {
 }
