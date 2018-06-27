f3ab2cb8b66dc2d702a9f36ab43c5351447ea9cd
John Stratton <jstrat0407@gmail.com>
2018-06-27 12:32:48 -0400

    added .wrapper html contents as a global variable

--- js/globals.js
+++ js/globals.js
@@ -1,5 +1,20 @@
 // global variables
 const names = ['Name', 'Address', 'Address 2', 'City', 'State', 'Zip', 'Purpose', 'Property Owner', 'Creation Date'];
+const wrapper = `
+<div class="input-group mr-auto ml-auto mb-3">
+	<div class="custom-file">
+		<input type="file" class="custom-file-input" id="inputGroupFile02">
+		<label class="custom-file-label" for="inputGroupFile02">Drag &amp; Drop or click here to browse for your file</label>
+	</div>
+	<div class="input-group-append" id="upload">
+		<span class="input-group-text">Upload</span>
+	</div>
+</div>
+<div class="text-center">
+	<input type="checkbox" id="headerCheck" checked aria-label="Checkbox if header is included">
+<label for="headerCheck">Check here if a header is included as the first line of the file</label>
+</div>
+`;
 let code;
 let editable;
 let message;
