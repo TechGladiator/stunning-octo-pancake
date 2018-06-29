c53b16b4b15b600f22a239a6ab1d3bc95d1bc8af
John Stratton <jstrat0407@gmail.com>
2018-06-29 19:40:25 -0400

    replace old key name with new key name

--- js/parse.js
+++ js/parse.js
@@ -25,22 +25,40 @@ function printStats(msg) {
     return;
   }
   for (let i = 0; i < fieldNames.length; i++) {
-    const e = fieldNames[i];
     validateFieldNames(fieldNames[i]);
     if (!name) {
       console.log(fieldNames[i], ' is invalid');
-      // buildTable(row = 'header');
       buttonName = 'Cancel';
       code = 'invalidHeader';
       fix = fixButton(code, buttonName);
       modal(code, `${fieldNames[i]} is an invalid header name. Replace with correct header: ${names[i]}?`, fix);
       modalDispose(code, buttonName);
       $(`#${code}Close2`).click(() => {
-        console.log('Field Name was: ', fieldNames[i]);
-        fieldNames[i] = names[i];
-        console.log('Field Name is now: ', fieldNames[i]);
+        $(`#${code}`).on('hidden.bs.modal', () => {
+          let oldKey = fieldNames[i];
+          fieldNames[i] = names[i];
+          let newKey = fieldNames[i];
+          console.log('Field Name was: ', oldKey);
+          console.log('Field Name is now: ', newKey);
+          fieldData.forEach((e) => {
+            for (const k in e) {
+              if (k == oldKey) {
+                e[newKey] = e[oldKey];
+                delete e[oldKey];
+              } else if (k == 'Address 2') {
+                e['Address2'] = e['Address 2'];
+                delete e['Address 2'];
+                e['Address 2'] = e['Address2'];
+                delete e['Address2'];
+              } else if (k == '__parsed_extra') {
+                e['parsed_extra'] = e['__parsed_extra'];
+                delete e['__parsed_extra'];
+              }
+            }
+          });
+          printStats('Key updated');
+        });
       });
-      console.log('return');
       return;
     }
   }
