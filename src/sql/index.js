958d8ec10d83a90ec2007548482119c07dec96ef
John Stratton <jstrat0407@gmail.com>
2018-07-23 11:31:53 -0400

    connecting app with database

--- src/sql/index.js
+++ src/sql/index.js
@@ -1,1 +1,23 @@
 // src/sql/index.js
+const { Pool, Client } = require('pg');
+
+// pools will use environment variables
+// for connection information
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'project',
+  password: 'secretpassword',
+  port: 5432,
+});
+
+const res = await pool.query('SELECT NOW()');
+await pool.end();
+
+// clients will also use environment variables
+// for connection information
+const client = new Client();
+await client.connect();
+
+const res = await client.query('SELECT NOW()');
+await client.end();
