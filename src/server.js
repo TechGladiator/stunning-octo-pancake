e957d18eb0ccea9b3c838d91440eaad475bdaee6
John Stratton <jstrat0407@gmail.com>
2018-07-24 10:25:44 -0400

    set pug as view engine

--- src/server.js
+++ src/server.js
@@ -11,6 +11,13 @@ const client = require('./sql');
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
+app.set('views', './src/views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 app.listen(config.port, function() {
   console.log(`${config.appName} is listening on port ${config.port}`);
 });
