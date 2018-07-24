21137a9a5439a2f2e644009eca7aa71e59b85323
John Stratton <jstrat0407@gmail.com>
2018-07-24 14:01:47 -0400

    wrapped code into main()

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,9 @@
-// Enable uploading and parsing of csv file
-$('#jumboHeader').addClass('mb-5');
-$('#jumboHeader').html('Upload or View Data');
-$('.wrapper').html(`${wrapper1}`);
-$('.csv').html('');
+function main() {
+  $('#jumboHeader').addClass('mb-5');
+  $('#jumboHeader').html('Upload or View Data');
+  $('.wrapper').html(`${wrapper1}`);
+  $('.csv').html('');
+  $('#uploadCSV').click(parseFile);
+}
 
-// Flag records that do not meet data type formats
-
-// Implement JavaScript methods for data validation
-
-// Highlight specific fields of records with data entry issues
-
-// Guide user in cleaning records
-
-// Correct errant fields or remove the record 
-
-// i.e. “District of Columbia” instead of “DC” in the state field should be flagged and user forced to correct field, delete the record, or cancel entire process
-
-// Once corrected, check again to ensure data type formats are met and flagged accordingly 
-
-// Alternatives: 
-
-// Wizard based process to do field clean up on records with problems only
-
-// Highlight records with issues and establish a repair-next button that jumps to each record with an issue 
-
-// Clear records that have been corrected as user goes
-
-// Application displays all successful records
-
-// Append the parsed and validated data to HTML document body as a table
-
-// Once all records are cleaned or removed, user is presented with an option to name and save the data set
-
-// Display the name of the imported data set on the grid header or top of page
+main();
