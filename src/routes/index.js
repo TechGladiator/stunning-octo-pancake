aa05fda2941f464d6426902a80e8e8daa56c60df
John Stratton <jstrat0407@gmail.com>
2018-07-25 11:47:25 -0400

    fixed ReferenceError: id is not defined

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const db = require('../sql');
 
 router.get('/search/:id', (req, res, next) => {
+  const id = req.params.id;
   db.query('SELECT name, address, address2, city, statecode, zip, purpose, "propertyOwner", "creationDate", lat, long FROM imports JOIN imported_data ON imports.importid=imported_data.importid WHERE importname = $1', [id], (err, res) => {
     if (err) {
       return next(err);
