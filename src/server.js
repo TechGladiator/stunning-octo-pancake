e4676819a2404c2d4418087dcad065a9c328766c
John Stratton <jstrat0407@gmail.com>
2018-07-31 13:49:49 -0400

    require body-parser
    also did a little clean up

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
 // src/server.js
-const path = require('path');
-const express = require('express');
-
+const bodyParser = require('body-parser');
 const config = require('./config');
+const cool = require('cool-ascii-faces');
+const db = require('./sql');
+const express = require('express');
+const path = require('path');
+const router = require('./routes');
 
 const app = express();
 
-const router = require('./routes')
-
-const pool = require('./sql');
-
-const cool = require('cool-ascii-faces');
-
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
 app.use('/api', router);
 
 app.set('views', './src/views');
