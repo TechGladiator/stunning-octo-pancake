3549fcce8fe45a5336ac561112037ea658d39d2d
John Stratton <jstrat0407@gmail.com>
2018-08-21 19:19:04 -0400

    defined factory files

---
+++ spec/factories/records.rb
@@ -0,0 +1,16 @@
+# spec/factories/records.rb
+FactoryBot.define do
+  factory :record do
+    name { Faker::StarWars.character }
+    address { Faker::Address.street_address }
+    address_2 { Faker::Address.secondary_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zip { Faker::Address.zip }
+    purpose { Faker::Book.genre }
+    property_owner { Faker::StarWars.character }
+    creation_date { Faker::Date.backward(14) }
+    done false
+    record_id nil
+  end
+end
