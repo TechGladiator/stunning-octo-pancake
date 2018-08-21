e417248889461fd86747b0b9afd1e2d19dd5dd2c
John Stratton <jstrat0407@gmail.com>
2018-08-21 19:43:18 -0400

    setup Import controller

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,2 +1,44 @@
+# app/controllers/imports_controller.rb
 class ImportsController < ApplicationController
+  before_action :set_import, only: [:show, :update, :destroy]
+
+  # GET /imports
+  def index
+    @imports = Import.all
+    json_response(@imports)
+  end
+
+  # POST /imports
+  def create
+    @import = Import.create!(import_params)
+    json_response(@import, :created)
+  end
+
+  # GET /imports/:id
+  def show
+    json_response(@import)
+  end
+
+  # PUT /imports/:id
+  def update
+    @import.update(import_params)
+    head :no_content
+  end
+
+  # DELETE /imports/:id
+  def destroy
+    @import.destroy
+    head :no_content
+  end
+
+  private
+
+  def import_params
+    # whitelist params
+    params.permit(:import_name)
+  end
+
+  def set_import
+    @import = Import.find(params[:id])
+  end
 end
