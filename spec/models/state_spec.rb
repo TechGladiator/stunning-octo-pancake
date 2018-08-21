40b59241c08a0246adf08ffc9dc167141633bc21
John Stratton <jstrat0407@gmail.com>
2018-08-21 18:00:03 -0400

    added model specs for State

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,5 +1,10 @@
+# spec/models/state_spec.rb
 require 'rails_helper'
 
+# Test suite for the State model
 RSpec.describe State, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation test
+  # ensure column name is present before saving
+  it { should validate_presence_of(:id) }
+  it { should validate_presence_of(:name) }
 end
