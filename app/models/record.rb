4cb8fb653f7e87331342d47621a73dad0b9f443c
John Stratton <jstrat0407@gmail.com>
2018-08-22 14:21:13 -0400

    removed validation for address_2
    as it can be empty

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,5 +3,5 @@ class Record < ApplicationRecord
   belongs_to :import
 
   #validation
-  validates_presence_of :name, :address, :address_2, :city, :state, :zip, :purpose, :property_owner, :creation_date, :lat, :long
+  validates_presence_of :name, :address, :city, :state, :zip, :purpose, :property_owner, :creation_date, :lat, :long
 end
