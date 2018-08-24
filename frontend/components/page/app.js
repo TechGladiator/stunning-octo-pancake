c3712fbf7dfa5310bbcd3dd4047c6deecc6bd1f7
John Stratton <jstrat0407@gmail.com>
2018-08-24 15:58:09 -0400

    added build table

--- frontend/components/page/app.js
+++ frontend/components/page/app.js
@@ -524,3 +524,86 @@ function newCSV() {
   geoClear();
   parseFile();
 }
+
+function buildTable(row) {
+  let fn = '';
+  let fd = '';
+  let fullAddress = '';
+  let addressList = [];
+
+  $('.csv').addClass('p-5');
+  
+  fn = getFieldNames(fn);
+
+  ({ fd, fullAddress } = getFieldData(fd, row, fullAddress, addressList));
+
+  // force correction of header names
+  if (row == 'header') {
+    fd = '';
+  }
+
+  $('#jumboHeader').removeClass('mb-5');
+  $('#jumboHeader').html(fileName);
+  $('title').html(fileName);
+  $('.wrapper').html('');
+
+  $('.csv').html(`
+                    <div class="btn-group d-flex justify-content-center mb-3" role="group" aria-label="button group">
+                      <button type="button" class="btn btn-secondary" id="editData">Edit Data</button>
+                      <button type="button" class="btn btn-secondary invisible" id="saveRecords">Save Records</button>
+                      <button type="button" class="btn btn-secondary" id="mapData">Map Imported Data</button>
+                      <button type="button" class="btn btn-secondary" id="repairNext">Repair Next Error</button>
+                      <button type="button" class="btn btn-secondary" id="search">New Search</button>
+                      <button type="button" class="btn btn-secondary" id="newCSV">Import New CSV File</button>
+                    </div>
+                    <div class="card">
+                      <div class="card-body">
+                        <table id="csvTable" class="table table-bordered">
+                          <thead>
+                            <tr class="headerRow">
+                              ${fn}
+                            </tr>
+                          </thead>
+                          <tbody>
+                            ${fd}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  `);
+
+  if (mapped) {
+    $('tbody').addClass('latlong');
+    $('#saveRecords').removeClass('invisible');
+    $('title').html(`${$('#jumboHeader').html()} Mapped`);
+  }
+
+  if (firstError == undefined) {
+    $('#repairNext').addClass('invisible');
+  } else {
+    $('#mapData').addClass('invisible');
+  }
+
+  $('#editData').click(() => {
+    toggleEditable(row);
+  });
+  $('#saveRecords').click(() => {
+    saveRecords();
+  });
+  $('#mapData').click(() => {
+    if (!mapped) {
+      initialize();
+    }
+    geoIterate(addressList);
+  });
+  $('#repairNext').click(() => {
+    updateFields(row);
+    printStats();
+  });
+  $('#search').click(() => {
+    newCSV();
+    pageSwitch = true;
+    main();
+  });
+  $('#newCSV').click(newCSV);
+}
