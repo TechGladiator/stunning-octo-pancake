8f656550ab5a7e33f16e83d8d6ba929bf0636bd7
John Stratton <jstrat0407@gmail.com>
2018-08-27 18:52:35 -0400

    added returnToList.js

---
+++ frontend/components/page/returnToList.js
@@ -0,0 +1,11 @@
+const $ = require("jquery");
+const { buildTable } = require("./app");
+
+function returnToList() {
+  $("#mapData").html("Return to list");
+  $("#mapData").click(() => {
+    errorCount = 0;
+    buildTable();
+  });
+}
+exports.returnToList = returnToList;
