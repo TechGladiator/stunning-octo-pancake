c11eff09f1f9bc522f001aea003981eb428ccb8b
John Stratton <jstrat0407@gmail.com>
2018-08-28 13:34:11 -0400

    added modal.js and modalDispose.js

--- frontend/components/page/app.js
+++ frontend/components/page/app.js
@@ -1,50 +1,12 @@
+const { modalDispose } = require("./modalDispose");
+
+const { modal } = require("./modal");
+
 let { errorCount, editable, mapped, fieldData, fieldNames, fieldErrors, firstError, end, start, rowCount, code, message, fileName, headerCheck, name, row, fullResults, pageSwitch, firstRun, fieldState, fieldZip, fieldDate, markers, geocoder, infowindow, map, lat, long } = require("./globalLet");
 
 const { names, wrapper2, wrapper1, mainTitle, wrapper3 } = require("./globalConst");
 
 const $ = require('jquery');
-// Modal
-
-function modal(moId, moBody, moFooter) {
-	$('body').append(`
-  <div class="modal fade" id="${moId}" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="${moId}Label" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="${moId}Label">${moId}</h5>
-          <button type="button" class="close" id="${moId}Close1" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body" id="modalBody"></div>
-        <div class="modal-footer" id="modalFooter"></div>
-      </div>
-    </div>
-	</div>
-	`);
-	$(`#${moId}`).modal('show');
-	$('#modalBody').html(`<h5 class="text-center">${moBody}</h5>`);
-	let okButton = `<button type="button" class="btn btn-primary" id="${moId}Close2">Ok</button>`;
-	if (moFooter) {
-		$('#modalFooter').html(`${moFooter}${okButton}`);
-	} else {
-		$('#modalFooter').html(okButton);
-	}
-	modalDispose(moId, 'Close1');
-	modalDispose(moId, 'Close2');
-}
-
-function modalDispose(moId, close, func) {
-	$(`#${moId}${close}`).click(() => {
-		$(`#${moId}`).modal('hide');
-		$(`#${moId}`).on('hidden.bs.modal', () => {
-			$('.modal').remove();
-			$('.modal-backdrop').remove();
-			if (func) func();
-		});
-	});
-}
-
 // fix errors
 
 function returnToList() {
