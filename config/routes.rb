a300ccdbaa521c4ea33202f7cbc1c125a97c3d75
John Stratton <jstrat0407@gmail.com>
2018-08-30 16:04:23 -0400

    updated import search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get '/imports/search', to: 'search#search'
   resources :imports do
     resources :records
+    get 'search', on: :collection
   end
 end
