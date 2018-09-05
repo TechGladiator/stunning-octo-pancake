47733bbbd0c982951d11da5e4d6df01c4f9dfa38
John Stratton <jstrat0407@gmail.com>
2018-09-05 13:40:13 -0400

    moved global variables into globalLets.js

--- frontend/components/page/app.js
+++ frontend/components/page/app.js
@@ -1,36 +1,9 @@
+let { errorCount, editable, mapped, fieldData, fieldNames, fieldErrors, firstError, end, start, rowCount, code, message, fileName, headerCheck, name, row, fullResults, pageSwitch, firstRun, fieldState, fieldZip, fieldDate, markers, geocoder, infowindow, map, lat, long } = require("./globalLets");
+
 const { names, wrapper2, wrapper1, mainTitle, wrapper3 } = require("./globalConsts");
 
 // global variables
 const $ = require('jquery');
-let code;
-let editable;
-let end;
-let errorCount = 0;
-let fieldData;
-let fieldDate = true;
-let fieldErrors;
-let fieldNames;
-let fieldState = true;
-let fieldZip = true;
-let fileName;
-let firstError;
-let firstRun = true;
-let fullResults;
-let geocoder;
-let headerCheck = true;
-let infowindow;
-let lat;
-let long;
-let map;
-let mapped = false;
-let markers = [];
-let message;
-let name = true;
-let pageSwitch = false;
-let row;
-let rowCount = 0;
-let start;
-
 // Modal
 
 function modal(moId, moBody, moFooter) {
