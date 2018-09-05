d9aa41bc499f015b1ed196fecdd25fca5f0d605d
John Stratton <jstrat0407@gmail.com>
2018-09-05 14:31:47 -0400

    moved code into showFilename() file

--- frontend/components/page/parseFile.js
+++ frontend/components/page/parseFile.js
@@ -1,10 +1,11 @@
+const { showFilename } = require("./showFilename");
+
 const { setPage } = require("./setPage");
 const { main } = require("./main");
 let {
   errorCount,
   firstError,
   rowCount,
-  fileName,
   headerCheck,
   pageSwitch
 } = require("./globalLets");
@@ -23,24 +24,7 @@ function parseFile() {
       headerCheck = false;
     }
   });
-  // replace input placeholder with file name
-  $("#inputGroupFile02").on("change", () => {
-    fileName = $(this).val();
-    fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
-    if (fileName !== "") {
-      $(this)
-        .next(".custom-file-label")
-        .addClass("selected")
-        .html(fileName);
-      $(".csv").html("");
-    } else {
-      $(this)
-        .next(".custom-file-label")
-        .addClass("selected")
-        .html("Drag & Drop or click here to browse for your file");
-      $(".csv").html("");
-    }
-  });
+  showFilename();
   $("#upload").click(() => {
     rowCount = 0;
     errorCount = 0;
