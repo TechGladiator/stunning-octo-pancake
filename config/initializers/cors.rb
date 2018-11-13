cc8935220f7b19bc5891a9daee51854217c85f29
John Stratton <jstrat0407@gmail.com>
2018-11-13 14:36:39 -0500

    added url for heroku app to allowed origins for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost'
+    origins 'https://stunning-octo-pancake-sencha.herokuapp.com/'
 
     resource '*',
       headers: :any,
